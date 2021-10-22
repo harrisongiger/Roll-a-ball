1061d47b174dd1cc5196e58fc6b6627d26598c51
harrisongiger <harrisongiger21@gmail.com>
2021-10-22 14:40:08 -0700

    lol got it rotating

--- Assets/Scripts/rotator.cs
+++ Assets/Scripts/rotator.cs
@@ -1,10 +1,10 @@
 using UnityEngine;
 using System.Collections;
 
-public class Rotator : MonoBehaviour {
+public class rotator : MonoBehaviour {
 
 	void Update () 
 	{
-		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+		transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
 	}
 }
